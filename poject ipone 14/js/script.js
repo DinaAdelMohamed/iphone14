77380275af09b171a4894db9fb79965c44c6e99a
DinaAdelMohamed <145399568+DinaAdelMohamed@users.noreply.github.com>
2023-09-29 02:26:38 +0300

    Add files via upload

---
+++ poject ipone 14/js/script.js
@@ -0,0 +1,8 @@
+let image=document.querySelector('#img');
+let conntainer=document.querySelector('.container')
+function phones(phone){
+image.src=phone
+}
+function colors(color){
+    conntainer.style.background=color
+}
